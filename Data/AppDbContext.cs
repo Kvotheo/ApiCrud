4ea35b9597f9e0af564d30b3a942c14a3a06f4f2
vitor <vitor@DESKTOP-67EGG34>
2024-03-17 17:03:47 -0300

    Adicionar arquivos de projeto.

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using ApiCrud.Estudantes;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiCrud.Data
+{
+    public class AppDbContext : DbContext
+    {
+       public DbSet<Estudante> Estudantes { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=Banco;Trusted_Connection=True;");
+            base.OnConfiguring(optionsBuilder);
+
+        }
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,24 @@
+using ApiCrud.Data;
+using ApiCrud.Estudantes;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddScoped<AppDbContext>();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+
+//Configurandao as rotas
+app.AddRotasEstudantes();
+
+app.Run();
